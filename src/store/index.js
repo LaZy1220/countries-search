d6d05dc832f5813a84648e92fc15766da848c78b
LaZy1220 <4elove4iska@gmail.com>
2022-07-25 15:22:42 +0300

    refact setCountries on redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,7 @@
-import {axios} from 'axios'
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-import * as api from '../config'
 
 export const store = createStore(rootReducer,composeWithDevTools(
-    applyMiddleware(
-        thunk.withExtraArgument({
-            client: axios,
-            api
-}))))
+    applyMiddleware(thunk)))
