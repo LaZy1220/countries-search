0f6f27ca12d785f329066a8506419320842b63bd
LaZy1220 <4elove4iska@gmail.com>
2022-07-28 10:12:22 +0300

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import {store} from './store/index'
+import {store,persistor} from './store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,16 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import storage from "redux-persist/lib/storage";
+import { persistStore,persistReducer } from "redux-persist";
 
-export const store = createStore(rootReducer,composeWithDevTools(
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['theme']
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+export const store = createStore(persistedReducer,composeWithDevTools(
     applyMiddleware(thunk)))
+export const persistor = persistStore(store)
